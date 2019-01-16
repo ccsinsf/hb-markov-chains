3eae98f6c51a791c1a6db5f791fdd4515351882c
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-15 17:51:59 -0800

    updated functions

--- markov.py
+++ markov.py
@@ -12,8 +12,8 @@ def open_and_read_file(file_path):
     # feed file_path(arg) to file.read() 
     # return str("contents")
 
-    print(open(file_path).read())
-    print(type(open(file_path).read()))
+    # print(open(file_path).read())
+    # print(type(open(file_path).read()))
     return open(file_path).read()
 
     # your code goes here
@@ -45,10 +45,72 @@ def make_chains(text_string):
         >>> chains[('there','juanita')]
         [None]
     """
+    # using value of function created above, we're going to split words with .split() with no arguments to break on whitespaces, linebreaks, spaces and tabs
+    # convert string to list
+    
+    word_list = open_and_read_file(input_path).split()
+    #print(word_list)
+    
+    bigram_list = []
+    value_list = []
 
     chains = {}
+    
+    # loop over list to generate tuples (bigrams)
+  
+    for i in range(len(word_list) -1):
+        # print([word_list[i], word_list[i+1]])
+        #new attempt: two loops with two different ranges
+        #step 1: get keys
+        bigram_list_prep = [word_list[i], word_list[i+1]]
+        word_tuple = tuple(bigram_list_prep)
+        bigram_list.append(word_tuple)
 
-    # your code goes here
+    for y in range(len(word_list) - 2):
+        value_list.extend([word_list[y+2]])
+
+        # create dictionary of keys as tuples
+        # find item(word) indexed one more than each bigram
+        # create a list of each of these indexed words (not a unique list)
+        # assign each list to its key in dictionary
+        # introduce condition .get()
+
+    # z = 0
+    # for z in range(len(bigram_list)):
+    # while z < len(bigram_list):
+    # for z in range(len(word_list) - 1):     
+    #     # for word_tuple in bigram_list:  
+    #     if chains[bigram_list[z]] in chains:
+    #         chains[bigram_list[z]] += list(value_list[z])
+    #     else:
+    #         chains[bigram_list[z]] = list(value_list[z]) 
+    #     # z += 1
+
+    z = 0
+
+    for word in bigram_list:
+        # chains[word] = chains.get(word) + extend(value_list[z])
+        if chains[word] in chains:
+            chains[word] = list(value_list[z])
+        else:
+            chains[word] = list(value_list[z])
+        z += 1
+
+    # for word in bigram_list:
+    #     chains[word] = chains.get(word, 0) + word
+
+    # goal is to print 
+    # print(bigram_list, value_list)
+    # print(value_list, type(value_list))
+    print(chains)    
+
+    # print(word_list)
+#push this to bigram_list
+
+    # convert list of tuples to dictionary keys in our "chains" dictionary
+    # values in our chains dictionary will be the words that could follow our keys as lists
+
+    
 
     return chains
 
